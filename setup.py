f7177291b7070d3945f07876d72701c1f9e34271
Elling <elling19@outlook.com>
2024-02-29 15:47:33 +0800

    feat(main): First commit.

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyruster',
-    version='0.1.1',
+    version='0.1.2',
     author='Elling',
     description='Implementing some syntax like rust.',
+    long_description=open('README.rst').read(),
     license='MIT',
     packages=find_packages(),
     install_requires=[
