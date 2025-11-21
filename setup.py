85840b072bc93a7e71063c3767405b9c79363380
elling <zhcshine@126.com>
2025-11-21 11:34:01 +0800

    feat(main): Update stub file.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyruster',
-    version='0.1.8',
+    version='0.1.9',
     author='Elling',
     description='Implementing some syntax like rust.',
     long_description=open('README.rst').read(),
